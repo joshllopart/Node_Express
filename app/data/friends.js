08528e7c2ee5a4d63522a25bbfc03e2da93ccff6
Joshua <joshuag38@gmail.com>
2019-03-03 12:41:02 -0600

    first commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,85 @@
-var Firends = [{
+var Friends = [
+     
+{
     
     name :"Ahmed",
-    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo :"https://picsum.photos/200",
     scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-  
-  
-}]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+{
+    
+    name :"Ahmed",
+    photo :"https://picsum.photos/200",
+    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
+
+},
+
+];
+
+module.exports = Friends;
--- server.js
+++ server.js
@@ -4,10 +4,19 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 4000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+console.log("Its working " + PORT);
+});
 
 
 
